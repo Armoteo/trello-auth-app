a637a2dc1bbdaab34646d95bb186d6846116aceb
Armoteo <armoteo@ukr.net>
2020-01-21 11:40:31 +0200

    getting boards

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from "react";
 import { setToLocaStorage, getFromLocalStorage } from '../../utils'
+
 
 const { REACT_APP_API_KEY, REACT_APP_APP_NAME, REACT_APP_APP_URL, REACT_APP_APP_SCOPE } = process.env
 const TOKEN_STORAGE_KEY = 'TOKEN';
@@ -16,20 +16,28 @@ interface AppState {
     board: Array<Board>;
 }
 export class App extends React.Component {
-    public state:AppState = {
+    public state: AppState = {
         token: '',
         board: []
-    }
+    };
 
     //save token in local storage
     private async setToken(token: string) {
-        this.setState({ token });
+       await this.setState({ token : token });
         await setToLocaStorage(TOKEN_STORAGE_KEY, token);
     }
-//geting token in local storage
-    private async getTokken() {
-        const token = await getFromLocalStorage(TOKEN_STORAGE_KEY);
-        return token;
+    //getting token in local storage
+    private   getToken() {
+        return  getFromLocalStorage(TOKEN_STORAGE_KEY);
+    }
+
+    private async  getBoard() {
+        const token = this.getToken();
+        const board = await fetch(`https://api.trello.com/1/members/me/boards?key=${REACT_APP_API_KEY}&token=${token}`);
+        if(board.ok){
+            let json = await board.json();
+            console.log(json);
+        }
     }
 
     private renderHeader() {
@@ -42,12 +50,13 @@ export class App extends React.Component {
     }
 
     private renderContent() {
+        this.getBoard();
         return (
             this.state.token ? <h2>Some secret content</h2> : 'Please log in'
         )
     }
 
-    //geting token from request location
+    //getting token from request location
     public async componentDidMount() {
         const newToken = window.location.hash.split('=')[1];
         this.setToken(newToken);
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,8 +1,8 @@
 //сохраняем токен в локал стор
 export const setToLocaStorage = async (key: string, data: any) => {
     window.localStorage.setItem(key, data);
-}
+};
 //извлекаем токен в локал стор
-export const getFromLocalStorage = async (key: string) => {
-    window.localStorage.getItem(key);
-}
+export const getFromLocalStorage =  (key: string) => {
+   return window.localStorage.getItem(key);
+};
