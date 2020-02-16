76b89581e1c1c2c58c1f35c6fafb79a5d6a9f265
Armoteo <armoteo@ukr.net>
2020-02-16 18:43:04 +0200

    ready functional cards(need refactor middle)

--- src/components/ListCard/ListCard.tsx
+++ src/components/ListCard/ListCard.tsx
@@ -21,54 +21,59 @@ const { REACT_APP_API_KEY } = process.env;
 
 class ListCard extends React.Component<any> {
 
+public state={
+    text: ''
+}
+
+
     componentDidMount() {
         this.props.onFetchBoards!();
     }
 
-    // private toggleListCardRight(idCard: any, idList: any) {
-    //     const listID = this.genericIdList(idList, 'Right');
-    //     this.toogleList(idCard, listID);
-    // }
+    private toggleListCardRight(idCard: any, idList: any) {
+        const listID = this.genericIdList(idList, 'Right');
+        this.toogleList(idCard, listID);
+    }
 
-    // private toggleListCardLeft(idCard: any, idList: any) {
-    //     // const listID = this.genericIdList(idList, 'Left');
-    //     // this.toogleList(idCard, listID);
-    // }
+    private toggleListCardLeft(idCard: any, idList: any) {
+        const listID = this.genericIdList(idList, 'Left');
+        this.toogleList(idCard, listID);
+    }
 
-    // private genericIdList = (idList: string, action: string) => {
+    private genericIdList = (idList: string, action: string) => {
 
-    //     let indexItem = this.props.lists.findIndex((el: any) => el.id === idList);
+        let indexItem = this.props.lists.findIndex((el: any) => el.id === idList);
 
-    //     if (indexItem < this.props.lists.length - 1 && action === 'Right') {
-    //         indexItem = indexItem + 1;
-    //         return this.props.lists[indexItem].id;
-    //     } else if (indexItem <= this.props.lists.length - 1 && action === 'Left' && indexItem !== 0) {
-    //         indexItem = indexItem - 1;
-    //         return this.props.lists[indexItem].id;
-    //     } else if (indexItem === 0 && action === 'Left') {
-    //         indexItem = this.props.lists.length - 1;
-    //         return this.props.lists[indexItem].id;
-    //     } else {
-    //         indexItem = 0;
-    //         return this.props.lists[indexItem].id;
-    //     }
-    // }
+        if (indexItem < this.props.lists.length - 1 && action === 'Right') {
+            indexItem = indexItem + 1;
+            return this.props.lists[indexItem].id;
+        } else if (indexItem <= this.props.lists.length - 1 && action === 'Left' && indexItem !== 0) {
+            indexItem = indexItem - 1;
+            return this.props.lists[indexItem].id;
+        } else if (indexItem === 0 && action === 'Left') {
+            indexItem = this.props.lists.length - 1;
+            return this.props.lists[indexItem].id;
+        } else {
+            indexItem = 0;
+            return this.props.lists[indexItem].id;
+        }
+    }
 
-    // private getToken = () => {
-    //     return getFromLocalStorage(APP_TOKEN);
-    // }
+    private getToken = () => {
+        return getFromLocalStorage(APP_TOKEN);
+    }
 
-    // private async toogleList(id: string, idList: any) {
-    //     const token = this.getToken();
-    //     const url = `https://api.trello.com/1/cards/${id}/idList?value=${idList}&key=${REACT_APP_API_KEY}&token=${token}`;
-    //     const response = await fetch(url, {
-    //         method: 'PUT'
-    //     });
-    //     if (response.ok === true && response.status === 200) {
-    //         this.props.onFetchBoards!();
-    //     }
-    // }
-    // listCardsArray
+    private async toogleList(id: string, idList: any) {
+        const token = this.getToken();
+        const url = `https://api.trello.com/1/cards/${id}/idList?value=${idList}&key=${REACT_APP_API_KEY}&token=${token}`;
+        const response = await fetch(url, {
+            method: 'PUT'
+        });
+        if (response.ok === true && response.status === 200) {
+            this.props.onFetchBoards!();
+        }
+    }
+    
 
     private toogleText(id: string) {
         const array = this.props.listCard.map((item: any) => {
@@ -82,36 +87,48 @@ class ListCard extends React.Component<any> {
             return item;
         });
         this.props.editCardStatus(array);
-        // const token = this.getToken();
-        // const text = 'Hello World'
-        // const url = `https://api.trello.com/1/cards/${id}/name?value=${text}&key=${REACT_APP_API_KEY}&token=${token}`;
-        // const response = await fetch(url, {
-        //     method: 'PUT'
-        // });
-        // if (response.ok === true && response.status === 200) {
-        //     this.props.onFetchBoards!();
-        // }
+        
+       
+    }
+
+    private textState(e:any){
+        this.setState({text:e.target.value});
+    }
+
+    private async setTextCards(id:string){
+    const token = this.getToken();
+        const text = this.state.text;
+        const url = `https://api.trello.com/1/cards/${id}/name?value=${text}&key=${REACT_APP_API_KEY}&token=${token}`;
+        const response = await fetch(url, {
+            method: 'PUT'
+        });
+        if (response.ok === true && response.status === 200) {
+            this.props.onFetchBoards!();
+        }
     }
 
     private createListItem() {
         return this.props.listCard.map((item: any, index: number) => {
+            
             const styleTextArea = item.flagTextArea === true ? "TextAreaCard anable" : "TextAreaCard disable";
+            const styleSaveButton = item.flagTextArea === true ? "anable" : "disable";
+            const styleEdit = item.flagTextArea === true ? "disable" : "anable";
 
             return item.idList === this.props.id ?
                 <div className="ItemListCard" key={index}>
                     <span>{item.name}</span>
-                    <textarea className={styleTextArea} placeholder={item.name}></textarea>
+                    <textarea className={styleTextArea} placeholder={item.name} onChange={(e)=>this.textState(e)}></textarea>
                     <div>
-                        <button type="button">
+                        <button type="button" onClick={()=>this.toggleListCardLeft(item.id, item.idList)}>
                             <i className="fas fa-arrow-left"></i>
                         </button>
-                        <button type="button">
+                        <button type="button" onClick={()=>this.toggleListCardRight(item.id, item.idList)}>
                             <i className="fas fa-arrow-right"></i>
                         </button>
-                        <button type="button" onClick={() => this.toogleText(item.id)}>
+                        <button type="button" className={styleEdit} onClick={() => this.toogleText(item.id)}>
                             <i className="fas fa-pencil-alt"></i>
                         </button>
-                        <button type="button" className='Button-save'>
+                        <button type="button" className={styleSaveButton} onClick={()=>this.setTextCards(item.id)}>
                             <i className="fas fa-save"></i>
                         </button>
                     </div>
